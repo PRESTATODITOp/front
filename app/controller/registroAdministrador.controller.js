b01de77fe2bd82b1607df928ae84b6835db3506e
Juan3728825 <jmmartinez4817@misena.edu.co>
2023-07-11 14:53:58 -0500

    redirect para las alertas

--- app/controller/registroAdministrador.controller.js
+++ app/controller/registroAdministrador.controller.js
@@ -6,46 +6,46 @@ const registroAdministrador = async(req,res)=>{
 }
 
 const registrarAdmi = async (req, res) => {
-  try {
-      const data = {
-          nombre: req.body.nombre,
-          apellido: req.body.apellido,
-          tipo_documento: req.body.tipo_documento,
-          id: req.body.numero_documento,
-          correo: req.body.correo_electronico,
-          telefono: req.body.telefono,
-          direccion: req.body.direccion,
-          jornada: req.body.jornada,
-          contrasena: req.body.contrasena,
-          id_rol: req.body.id_rol,
-          programa_formacion: "null",
-          numero_ficha: "null"
-      };
+    try {
+        const data = {
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            tipo_documento: req.body.tipo_documento,
+            id: req.body.numero_documento,
+            correo: req.body.correo_electronico,
+            telefono: req.body.telefono,
+            direccion: req.body.direccion,
+            jornada: req.body.jornada,
+            contrasena: req.body.contrasena,
+            id_rol: req.body.id_rol,
+            programa_formacion: "null",
+            numero_ficha: "null"
+        };
 
-      const url = "http://localhost:3000/api/usuario";
-      const options = {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(data)
-      };
+        const url = "http://localhost:3000/api/usuario";
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        };
 
-      const response = await fetch(url, options);
-      const responseData = await response.json();
+        const response = await fetch(url, options);
+        const responseData = await response.json();
 
-      if (responseData && responseData > 0) {
-          // Registro exitoso
-          res.send(responseData);
-      } else {
-          // Error en el registro
-          res.send(false);
-      }
-  } catch (error) {
-      console.error(error);
-      // Error en la petición
-      res.send(false);
-  }
+        if (responseData && responseData > 0) {
+            // Registro exitoso
+            res.redirect("/rol");
+        } else {
+            // Error en el registro
+            res.redirect("/?alerta=1");
+        }
+    } catch (error) {
+        console.error(error);
+        // Error en la petición
+        res.redirect("/?alerta=2");
+    }
 };
 
 
